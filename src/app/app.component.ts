eb7740105ea148b1fd8b432c617a47d9bdea4028
SimonDMaxwell <lolicongress@outlook.com>
2022-05-17 01:16:03 -0500

    Menu Request Executed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { menu_item } from './interfaces/meal_option';
+import { OrderService } from './services/order.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  Menu: menu_item[] | null | undefined;
+  constructor (private orderS: OrderService) {
+
+  }
+
+  ngOnInit(): void {
+    this.orderS.getMenu().subscribe((data) => {
+      this.Menu = data;
+    })
+  }
   title = 'CLS';
 }
